ea5c4686871c637faaed4d39ccfd5a917a7fb2b8
Amanda Atkins <amanda@digitalrainstorm.com>
2020-01-05 14:15:09 -0800

    Add functionality to render PDF and save to server

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 const axios = require('axios');
 const inquirer = require('inquirer');
+const html = require('./html.js');
+const electron = require('electron-html-to');
+
+var sendToPDF = electron({
+    converterPath: electron.converters.PDF,
+    allowLocalFilesAccess: true
+});
 
 var favColor;
 var userName;
+var pdfContent;
 
 inquirer.prompt([
     {
@@ -15,7 +23,7 @@ inquirer.prompt([
         type: 'list',
         name: 'favColor',
         message: 'What is your favorite color?',
-        choices: ['Pink','Red','Blue','Green','Purple','Orange','Yellow']
+        choices: ['Pink','Red','Blue','Green','Purple','Orange']
     }
 ]).then(answers => {
     userName = answers.username;
@@ -26,6 +34,29 @@ inquirer.prompt([
 function fetchProfile() {
     var query = "https://api.github.com/users/"+userName;
     axios.get(query).then(response => {
-        console.log(response);
+        var userData = response.data;
+        query += "/starred"; 
+        axios.get(query).then(starredResponse => {
+            pdfContent = html.renderPdfContent(userData, favColor, starredResponse.data);
+            renderPDF();
+        });
+    });
+}
+
+function renderPDF() {
+    console.log('rendering pdf...');
+    sendToPDF({
+        html: pdfContent,
+        pdf: {
+            pageSize: 'Letter',
+            printBackground: true
+        }
+    }, (err, result) => {
+        if (err) {
+            return console.log(err);
+        }
+        result.stream.pipe(fs.createWriteStream('profiles/'+userName+'.pdf'));
+        console.log('success!');
+        sendToPDF.kill();
     });
 }
